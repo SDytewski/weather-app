51c6c20c1f80755210d274137cb059f9673a9fa1
Steve Dytewski <dy2ski@gmail.com>
2025-06-16 20:54:54 -0700

    styled search bar

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png'
+
 
 export const Weather = () => {
     return (
-        <div>
-            <h1>
-                Weather
-            </h1>
-
+        <div className="weather">
+            <div className="search-bar">
+                <input type="text" placeholder='Search' />
+                <img src={search_icon} className="my-icon"  alt="" />
+            </div>
         </div>
     )
 }
