47658a92b10e739359ae0b4a7c0ccc4d242fec67
Steve Dytewski <dy2ski@gmail.com>
2025-07-02 15:37:15 -0700

    added imports for icons

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
-
+import cloud_icon from '../assets/cloud.png'
+import cloudy_icon from '../assets/cloudy.png'
+import rain_icon from '../assets/rainy-day.png'
+import snow_icon from '../assets/snowflake.png'
+import sun_icon from '../assets/sun.png'
+import thunder_icon from '../assets/thunderstorm.png'
+import sun2_icon from '../assets/sun2.png'
+import wind_icon from '../assets/wind.png'
 
 export const Weather = () => {
     return (
