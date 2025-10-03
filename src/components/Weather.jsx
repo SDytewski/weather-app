b3e9726b604b197632255acfa93e818da055ef70
Steve Dytewski <dy2ski@gmail.com>
2025-10-02 22:31:43 -0700

    added an alert if string city is empty

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -35,6 +35,10 @@ export const Weather = () => {
 
 
     const search = async (city) => {
+        if( city === '') {
+            alert("Enter City Name");
+            return;
+        }
         try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${import.meta.env.VITE_APP_ID}`
 
